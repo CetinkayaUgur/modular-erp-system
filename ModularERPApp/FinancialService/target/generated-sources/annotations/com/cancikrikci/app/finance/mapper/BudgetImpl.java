79237aee9682776c0a85d4d2955ef2e47daf5724
can <88548505+Mellivoracc@users.noreply.github.com>
2025-05-26 11:40:01 +0300

    The basic structure of the project was created

---
+++ ModularERPApp/FinancialService/target/generated-sources/annotations/com/cancikrikci/app/finance/mapper/BudgetImpl.java
@@ -0,0 +1,28 @@
+package com.cancikrikci.app.finance.mapper;
+
+import com.cancikrikci.app.finance.dto.BudgetDTO;
+import com.cancikrikci.app.finance.entity.Budget;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-05-22T10:23:14+0300",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 19 (Oracle Corporation)"
+)
+@Component
+public class BudgetImpl implements IBudgetMapper {
+
+    @Override
+    public BudgetDTO toBudgetDTO(Budget budget) {
+        if ( budget == null ) {
+            return null;
+        }
+
+        BudgetDTO budgetDTO = new BudgetDTO();
+
+        budgetDTO.total = budget.total;
+
+        return budgetDTO;
+    }
+}
